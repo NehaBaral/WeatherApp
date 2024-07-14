71b352295c5f9251913d38539c0562c2f9ea7a73
kinju <mekinju@gmail.com>
2024-07-14 01:13:14 -0700

    chore: image and celius farehn updated

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -1,45 +1,52 @@
-//
-//  ViewController.swift
-//  WeatherApp
-//
-//  Created by Neha on 2024-07-10.
-//
-
 import UIKit
 import CoreLocation
-import SwiftUI
+
 class ViewController: UIViewController {
-    
+
     @IBOutlet var searchIcon: UIImageView!
+    @IBOutlet var weatherImage: UIImageView!
     @IBOutlet var searchBar: UISearchBar!
     @IBOutlet var locationItem: UIBarButtonItem!
-
     @IBOutlet weak var city: UILabel!
     @IBOutlet weak var weatherCondition: UILabel!
-
+    @IBOutlet weak var temperature: UILabel!
+    @IBOutlet var switchButton: UISegmentedControl!
     var weatherData = WeatherDataService()
-
     private let locationManager = CLLocationManager()
-    
+    var citiesWeather: [weatherDataObject] = []
+    var isCelsius: Bool = true
+    var currentWeatherInfo: weatherDataObject?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         searchBar.delegate = self
         weatherData.delegate = self
     
         self.navigationItem.titleView = searchBar
         self.navigationItem.leftBarButtonItem = locationItem
 
-        //Remove the searchIcon from the left position of search bar
+        // Remove the searchIcon from the left position of the search bar
         searchBar.setImage(UIImage(), for: .search, state: .normal)
         
-        //Alignment of text from right to left
+        // Align text from right to left
         searchBar.searchTextField.textAlignment = .right
         searchBar.searchTextField.placeholder = "Search Location"
         
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: searchIcon)
         
         locationManager.delegate = self
+        
+        // Set up segmented control action
+        switchButton.addTarget(self, action: #selector(segmentedControlValueChanged(_:)), for: .valueChanged)
+        
+        // Add tap gesture recognizer to the search icon
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(searchIconTapped))
+        searchIcon.isUserInteractionEnabled = true
+        searchIcon.addGestureRecognizer(tapGestureRecognizer)
+        
+        // Request initial location to display weather info
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
     }
     
     @IBAction func locationButton(_ sender: UIBarButtonItem) {
@@ -52,43 +59,96 @@ class ViewController: UIViewController {
     }
 
     @objc func weatherDetails(location: String) {
-        weatherData.getWeatherApi(coordinates:location)
+        weatherData.getWeatherApi(coordinates: location)
     }
 
-}
-
-extension ViewController : weatherManagerDelegate {
-    func didFailWithError(info: any Error) {
-        print("Error: \(info)")
-
+    func addCityWeather(_ weather: weatherDataObject) {
+        citiesWeather.append(weather)
     }
     
-    func didUpdaterWeatherInformation(info: weatherDataObject) {
-        DispatchQueue.main.async {
-            print(info)
-            self.weatherCondition.text = info.current.condition.text
-            self.city.text = info.location.name
+    @IBAction func showCities(_ sender: UIBarButtonItem) {
+        let citiesVC = SecondScreenViewController()
+        citiesVC.citiesWeather = citiesWeather
+        citiesVC.isCelsius = isCelsius
+        navigationController?.pushViewController(citiesVC, animated: true)
+    }
+    
+    @objc func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+        isCelsius = sender.selectedSegmentIndex == 0
+        updateTemperatureLabel()
+    }
+    
+    private func updateTemperatureLabel() {
+        if let info = currentWeatherInfo {
+            temperature.text = isCelsius ? String(info.current.temp_c) : String(info.current.temp_f)
         }
+    }
+    
+    @objc func searchIconTapped() {
+        if let searchText = searchBar.text, !searchText.isEmpty {
+            weatherDetails(location: searchText)
+        }
+        searchBar.resignFirstResponder()
     }
 }
 
+extension ViewController: WeatherManagerDelegate {
+    func didFailWithError(error: Error) {
+        print("Error: \(error)")
+    }
+    
+    func didUpdateWeatherInformation(info: weatherDataObject) {
+        DispatchQueue.main.async {
+            self.currentWeatherInfo = info
+            self.weatherCondition.text = info.current.condition.text
+            self.city.text = info.location.name
+            self.addCityWeather(info)
+            self.updateTemperatureLabel()
+            
+            var iconURLString = info.current.condition.icon
+            if !iconURLString.hasPrefix("http://") && !iconURLString.hasPrefix("https://") {
+                iconURLString = "https:" + iconURLString
+            }
+            
+            if let iconURL = URL(string: iconURLString) {
+                self.loadImage(from: iconURL)
+            }
+        }
+    }
+    
+    private func loadImage(from url: URL) {
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data, error == nil {
+                DispatchQueue.main.async {
+                    self.weatherImage.image = UIImage(data: data)
+                }
+            } else {
+                print("Error loading image: \(String(describing: error))")
+            }
+        }
+        task.resume()
+    }
+}
 
-extension ViewController : CLLocationManagerDelegate {
+extension ViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last {
             let latitude = location.coordinate.latitude
             let longitude = location.coordinate.longitude
             displayLocation(locationText: "\(latitude),\(longitude)")
-
         }
     }
     
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print(error)
     }
 }
 
-extension ViewController : UISearchBarDelegate{
-    
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if let searchText = searchBar.text, !searchText.isEmpty {
+            weatherDetails(location: searchText)
+        }
+        searchBar.resignFirstResponder()
+    }
 }
-
--- WeatherApp/weatherData.swift
+++ WeatherApp/weatherData.swift
@@ -1,52 +1,55 @@
-//
-//  weatherData.swift
-//  WeatherApp
-//
-//  Created by Sangeetha Thyagarajan on 2024-07-12.
-//
-
 import Foundation
-import UIKit
 
-
-protocol weatherManagerDelegate {
-    func didUpdaterWeatherInformation(info: weatherDataObject)
-    func didFailWithError(info: Error)
+protocol WeatherManagerDelegate: AnyObject {
+    func didUpdateWeatherInformation(info: weatherDataObject)
+    func didFailWithError(error: Error)
 }
-var APIKey = "abbac59e4cf84b918c3142625241207"
+
+let APIKey = "abbac59e4cf84b918c3142625241207"
 
 struct WeatherDataService {
-    var delegate: weatherManagerDelegate?
-    var baseURL = "http://api.weatherapi.com/v1/current.json?key=\(APIKey)&q="
+    weak var delegate: (any WeatherManagerDelegate)?
+    let baseURL = "http://api.weatherapi.com/v1/current.json?key=\(APIKey)&q="
     
     func getWeatherApi(coordinates: String) {
         guard let url = URL(string: "\(baseURL)\(coordinates)") else {
+            let error = NSError(domain: "Invalid URL", code: 400, userInfo: nil)
+            delegate?.didFailWithError(error: error)
             return
         }
         
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = nil
         
         let task = URLSession.shared.dataTask(with: request) { data, _, error in
-            guard let data = data, error == nil else {
+            if let error = error {
+                DispatchQueue.main.async {
+                    self.delegate?.didFailWithError(error: error)
+                }
                 return
             }
             
-                let decoder = JSONDecoder()
-               do {
-                   let response: weatherDataObject = try decoder.decode(weatherDataObject.self, from: data)
-                    self.delegate?.didUpdaterWeatherInformation(info: response)
+            guard let data = data else {
+                let error = NSError(domain: "No data received", code: 500, userInfo: nil)
+                DispatchQueue.main.async {
+                    self.delegate?.didFailWithError(error: error)
                 }
-             
-            catch {
-                print(error)
-                self.delegate?.didFailWithError(info: error)
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                let response = try decoder.decode(weatherDataObject.self, from: data)
+                DispatchQueue.main.async {
+                    self.delegate?.didUpdateWeatherInformation(info: response)
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    self.delegate?.didFailWithError(error: error)
+                }
             }
         }
         task.resume()
     }
-   
 }
-
